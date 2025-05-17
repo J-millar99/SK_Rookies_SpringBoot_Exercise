599d9b80ac8f33ddbd09b7efaf9f196f378b17aa
J-millar99 <wlwogus33@gmail.com>
2025-05-18 07:37:57 +0900

    1-1

--- src/main/java/com/rookies3/myspringbootlab/runner/MyPropRunner.java
+++ src/main/java/com/rookies3/myspringbootlab/runner/MyPropRunner.java
@@ -1,6 +1,8 @@
 package com.rookies3.myspringbootlab.runner;
 
 import com.rookies3.myspringbootlab.properties.MyPropProperties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
@@ -18,6 +20,7 @@ public class MyPropRunner implements ApplicationRunner {
     @Value("${myprop.port}")
     private int port;
 
+
 //    @Override
 //    // 테스트 1 : @Vaule 어노테이션으로 가져오기
 //    public void run(ApplicationArguments args) throws Exception {
@@ -32,10 +35,11 @@ public class MyPropRunner implements ApplicationRunner {
 //        System.out.println("port: " + myPropProperties.getPort());
 //    }
 
+    private Logger logger = LoggerFactory.getLogger(MyPropRunner.class);
     @Override
     // 테스트 3 : System.out.println을 logger 메서드로 변경하기
     public void run(ApplicationArguments args) throws Exception {
-        System.out.println("userName: " + myPropProperties.getUserName());
-        System.out.println("port: " + myPropProperties.getPort());
+        logger.debug("userName: " + myPropProperties.getUserName());
+        logger.info("port: " + myPropProperties.getPort());
     }
 }
